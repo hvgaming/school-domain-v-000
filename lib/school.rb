c5a853e9fcbefaf7b55bcd73c8b37bf6d5272149
hugo valois <hvgaming@yahoo.com>
2018-10-01 19:16:40 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,24 +2,20 @@ class School
   attr_accessor :student, :grade, :roster 
   
   def initialize(school_name)
-    #@school_name = school_name
-    #empty hash 
     @roster = {}
   end
   
   def add_student(student, grade)
-    #If the grade doesn't exsist then create an empty key/pair and push grade key value into it 
+     into it 
     if roster[grade]== nil 
         roster[grade] = []
         roster[grade] << student
     else
-      #add students to exsisting grade 
     roster[grade] << student
   end
 end
 
  def grade(grade)
-   #retrieve all values from certain grade 
     roster[grade]
  end
   
